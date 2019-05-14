a36101a5cb9adab4027b6f12e3058b18b87a7bda
vabhishek-me <abhishek.k.v76076@gmail.com>
2019-05-15 03:30:51 +0530

    button hide logic

--- src/components/HelloPWA.js
+++ src/components/HelloPWA.js
@@ -15,15 +15,14 @@ const HelloPWA = () => {
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
         setDeferredPrompt(null);
-      });
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+    });
   };
 
   return (
@@ -32,7 +31,11 @@ const HelloPWA = () => {
         <div className="container">
           <h1 className="title">Hello PWA!</h1>
           <h2 className="subtitle">Welcome to React PWA.</h2>
-          <button type="button" onClick={installApp} className="button is-white">Install App</button>
+          {deferredPrompt && (
+            <button type="button" onClick={installApp} className="button is-white">
+              Install App
+            </button>
+          )}
         </div>
       </div>
     </section>
